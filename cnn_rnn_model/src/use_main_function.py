68d542b478a3e60f8cfe73a6c233f5467213f521
xiaojitui <tomking576@gmail.com>
2020-02-16 18:14:55 -0500

    Create use_main_function.py

---
+++ cnn_rnn_model/src/use_main_function.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import tensorflow as tf
+
+
+# In[ ]:
+
+
+
+
+
+# In[16]:
+
+
+tf.reset_default_graph()
+get_ipython().run_line_magic('run', 'main.py')
+
+
+# In[ ]:
+
+
+
+
+
+# In[17]:
+
+
+tf.reset_default_graph()
+get_ipython().run_line_magic('run', 'main.py --beamsearch')
+
+# --wordbeamsearch
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
